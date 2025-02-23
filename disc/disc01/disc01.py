88c4625febe8e41d9d8e6c9052c27805f414f606
MorningStar3426 <morningstarcdz@outlook.com>
2025-02-23 16:35:29 +0800

    commit code

---
+++ disc/disc01/disc01.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 11:59:29 2025
+
+@author: morni
+"""
+
+def race(x, y):
+    """The tortoise always walks x feet per minute, while the hare repeatedly
+    runs y feet per minute for 5 minutes, then rests for 5 minutes. Return how
+    many minutes pass until the tortoise first catches up to the hare.
+
+    >>> race(5, 7)  # After 7 minutes, both have gone 35 steps
+    7
+    >>> race(2, 4) # After 10 minutes, both have gone 20 steps
+    10
+    """
+    assert y > x and y <= 2 * x, 'the hare must be fast but not too fast'
+    tortoise, hare, minutes = 0, 0, 0
+    while minutes == 0 or tortoise - hare:
+        tortoise += x
+        if minutes % 10 < 5:
+            hare += y
+        minutes += 1
+    return minutes
